faacb24ebe202ef501b2803c7bb846f93140e028
kaykay6t3 <105970941+kaykay6t3@users.noreply.github.com>
2023-05-10 23:28:37 +0530

    base
    base data

---
+++ src/books.js
@@ -0,0 +1,24 @@
+import img2 from "./images/image-2.jpg";
+import img3 from "./images/image-3.jpg";
+import img1 from "./images/image-1.jpg";
+
+export const books = [
+  {
+    author: "Jordan Moore",
+    title: "Interesting Facts For Curious Minds",
+    img: img1,
+    id: 1,
+  },
+  {
+    author: "James Clear",
+    title: "Atomic Habits",
+    img: img2,
+    id: 2,
+  },
+  {
+    author: "Stephen King",
+    title: "Fairy Tale",
+    img: img3,
+    id: 3,
+  },
+];
